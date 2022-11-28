b614df8e0ab5cf79a039021e4ca3d89123b76498
FengXin <1638547819@qq.com>
2022-11-28 23:56:33 +0800

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,132 +1,441 @@
-BEGIN;
-DROP DATABASE IF EXISTS website_xixifu;
-CREATE DATABASE website_xixifu;
-use website_xixifu;
+CREATE DATABASE  IF NOT EXISTS `website_xixifu` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `website_xixifu`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: website_xixifu
+-- ------------------------------------------------------
+-- Server version	5.7.40-log
 
-CREATE TABLE user(
-	id int primary key auto_increment,
-	register_method varchar(115),
-	register_school varchar(110),
-	student_id INT NOT NULL,
-	student_name varchar(140),
-	contact varchar(140),
-	password varchar(140)
-);
-CREATE TABLE notice(
-	id int primary key auto_increment,
-	source varchar(120),
-	topic varchar(150),
-	date varchar(120),
-	content varchar(1500)
-);
-CREATE TABLE checkin(
-	id int primary key auto_increment,
-	course varchar(120),
-	teacher varchar(120),
-	time varchar(120),
-	endtime varchar(120)
-);
-CREATE TABLE checkin_record(
-	id int primary key auto_increment,
-	checkin_id varchar(120),
-	student_id varchar(120),
-	time varchar(120)
-);
-CREATE TABLE discuss(
-	id int primary key auto_increment,
-	from_teacher int,
-	source varchar(120),
-	topic varchar(150),
-	date varchar(120),
-	content varchar(1500)
-);
-CREATE TABLE discuss_record(
-	id int primary key auto_increment,
-	discuss_id int,
-	source varchar(120),
-	content varchar(1500)
-);
-CREATE TABLE exam(
-	id int primary key auto_increment,
-	course varchar(120),
-	name varchar(150),
-	date int
-);
-CREATE TABLE exam_record(
-	id int primary key auto_increment,
-	exam_id int,
-	student_id int,
-	date int,
-	score int
-);
-CREATE TABLE exam_question(
-	id int primary key auto_increment,
-	exam_id int,
-	question_id int,
-	content varchar(150)
-);
-CREATE TABLE exam_content(
-	id int primary key auto_increment,
-	exam_id int,
-	question_id int,
-	optionX varchar(15),
-	is_correct int,
-	content varchar(150)
-);
-CREATE TABLE exam_detail(
-	id int primary key auto_increment,
-	record_id int,
-	question_id int,
-	student_option varchar(15)
-);
-CREATE TABLE homework(
-	id int primary key auto_increment,
-	course varchar(120),
-	content varchar(1500)
-);
-CREATE TABLE homework_record(
-	id int primary key auto_increment,
-	homework_id int,
-	student_id int,
-	content varchar(12000),
-	existfile int,
-	filename varchar(130)
-);
-CREATE TABLE material(
-	id int primary key auto_increment,
-	course varchar(120),
-	name varchar(150)
-);
-CREATE TABLE courses(
-	id int primary key auto_increment,
-	course varchar(120),
-	teacher varchar(120)
-);
-CREATE TABLE teachers(
-	id int primary key auto_increment,
-	teacher_school varchar(120),
-	teacher_id varchar(120),
-    teacher_name varchar(120),
-    password varchar(120)
-);
-INSERT INTO teachers (teacher_school,teacher_id,teacher_name,password) 
-	VALUES ('吉林大学','21201106','胡嘉仪','hjyteacher');
-INSERT INTO teachers (teacher_school,teacher_id,teacher_name,password) 
-	VALUES ('吉林大学','123','刘淼','lmteacher');
-INSERT INTO teachers (teacher_school,teacher_id,teacher_name,password) 
-	VALUES ('北京大学','123','张三','zsteacher');
-INSERT INTO teachers (teacher_school,teacher_id,teacher_name,password) 
-	VALUES ('清华大学','123','李四','lsteacher');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO courses (teacher,course) 
-	VALUES ('胡嘉仪','爱情心理学');
-INSERT INTO courses (teacher,course) 
-	VALUES ('胡嘉仪','气人必修课');
-INSERT INTO courses (teacher,course) 
-	VALUES ('刘淼','web程序设计');
-INSERT INTO courses (teacher,course) 
-	VALUES ('刘淼','ACM程序设计竞赛');
-    
+--
+-- Table structure for table `checkin`
+--
 
+DROP TABLE IF EXISTS `checkin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checkin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course` varchar(120) DEFAULT NULL,
+  `teacher` varchar(120) DEFAULT NULL,
+  `time` varchar(120) DEFAULT NULL,
+  `endtime` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-COMMIT;
+--
+-- Dumping data for table `checkin`
+--
+
+LOCK TABLES `checkin` WRITE;
+/*!40000 ALTER TABLE `checkin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `checkin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checkin_record`
+--
+
+DROP TABLE IF EXISTS `checkin_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checkin_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `checkin_id` varchar(120) DEFAULT NULL,
+  `student_id` varchar(120) DEFAULT NULL,
+  `time` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checkin_record`
+--
+
+LOCK TABLES `checkin_record` WRITE;
+/*!40000 ALTER TABLE `checkin_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `checkin_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course` varchar(120) DEFAULT NULL,
+  `teacher` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (1,'爱情心理学','胡嘉仪'),(2,'气人必修课','胡嘉仪'),(3,'web程序设计','刘淼'),(4,'ACM程序设计竞赛','刘淼');
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `discuss`
+--
+
+DROP TABLE IF EXISTS `discuss`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `discuss` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_teacher` int(11) DEFAULT NULL,
+  `source` varchar(120) DEFAULT NULL,
+  `topic` varchar(150) DEFAULT NULL,
+  `date` varchar(120) DEFAULT NULL,
+  `content` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `discuss`
+--
+
+LOCK TABLES `discuss` WRITE;
+/*!40000 ALTER TABLE `discuss` DISABLE KEYS */;
+/*!40000 ALTER TABLE `discuss` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `discuss_record`
+--
+
+DROP TABLE IF EXISTS `discuss_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `discuss_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `discuss_id` int(11) DEFAULT NULL,
+  `source` varchar(120) DEFAULT NULL,
+  `content` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `discuss_record`
+--
+
+LOCK TABLES `discuss_record` WRITE;
+/*!40000 ALTER TABLE `discuss_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `discuss_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam`
+--
+
+DROP TABLE IF EXISTS `exam`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course` varchar(120) DEFAULT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam`
+--
+
+LOCK TABLES `exam` WRITE;
+/*!40000 ALTER TABLE `exam` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exam` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_content`
+--
+
+DROP TABLE IF EXISTS `exam_content`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exam_content` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `optionX` varchar(15) DEFAULT NULL,
+  `is_correct` int(11) DEFAULT NULL,
+  `content` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_content`
+--
+
+LOCK TABLES `exam_content` WRITE;
+/*!40000 ALTER TABLE `exam_content` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exam_content` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_detail`
+--
+
+DROP TABLE IF EXISTS `exam_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exam_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `record_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `student_option` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_detail`
+--
+
+LOCK TABLES `exam_detail` WRITE;
+/*!40000 ALTER TABLE `exam_detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exam_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_question`
+--
+
+DROP TABLE IF EXISTS `exam_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exam_question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `content` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_question`
+--
+
+LOCK TABLES `exam_question` WRITE;
+/*!40000 ALTER TABLE `exam_question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exam_question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_record`
+--
+
+DROP TABLE IF EXISTS `exam_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exam_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) DEFAULT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `date` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_record`
+--
+
+LOCK TABLES `exam_record` WRITE;
+/*!40000 ALTER TABLE `exam_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exam_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `homework`
+--
+
+DROP TABLE IF EXISTS `homework`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `homework` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course` varchar(120) DEFAULT NULL,
+  `content` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `homework`
+--
+
+LOCK TABLES `homework` WRITE;
+/*!40000 ALTER TABLE `homework` DISABLE KEYS */;
+/*!40000 ALTER TABLE `homework` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `homework_record`
+--
+
+DROP TABLE IF EXISTS `homework_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `homework_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `homework_id` int(11) DEFAULT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `content` varchar(12000) DEFAULT NULL,
+  `existfile` int(11) DEFAULT NULL,
+  `filename` varchar(130) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `homework_record`
+--
+
+LOCK TABLES `homework_record` WRITE;
+/*!40000 ALTER TABLE `homework_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `homework_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course` varchar(120) DEFAULT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notice`
+--
+
+DROP TABLE IF EXISTS `notice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `source` varchar(120) DEFAULT NULL,
+  `topic` varchar(150) DEFAULT NULL,
+  `date` varchar(120) DEFAULT NULL,
+  `content` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notice`
+--
+
+LOCK TABLES `notice` WRITE;
+/*!40000 ALTER TABLE `notice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `teacher_school` varchar(120) DEFAULT NULL,
+  `teacher_id` varchar(120) DEFAULT NULL,
+  `teacher_name` varchar(120) DEFAULT NULL,
+  `password` varchar(120) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'吉林大学','21201106','胡嘉仪','hjyteacher'),(2,'吉林大学','123','刘淼','lmteacher'),(3,'北京大学','123','张三','zsteacher'),(4,'清华大学','123','李四','lsteacher'),(5,'吉林大学','21201106','胡嘉仪','hjyteacher'),(6,'吉林大学','123','刘淼','lmteacher'),(7,'北京大学','123','张三','zsteacher'),(8,'清华大学','123','李四','lsteacher'),(9,'吉林大学','21201106','胡嘉仪','hjyteacher'),(10,'吉林大学','123','刘淼','lmteacher'),(11,'北京大学','123','张三','zsteacher'),(12,'清华大学','123','李四','lsteacher'),(13,'吉林大学','21201106','胡嘉仪','hjyteacher'),(14,'吉林大学','123','刘淼','lmteacher'),(15,'北京大学','123','张三','zsteacher'),(16,'清华大学','123','李四','lsteacher'),(17,'吉林大学','21201106','胡嘉仪','hjyteacher'),(18,'吉林大学','123','刘淼','lmteacher'),(19,'北京大学','123','张三','zsteacher'),(20,'清华大学','123','李四','lsteacher'),(21,'吉林大学','21201106','胡嘉仪','hjyteacher'),(22,'吉林大学','123','刘淼','lmteacher'),(23,'北京大学','123','张三','zsteacher'),(24,'清华大学','123','李四','lsteacher');
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `register_method` varchar(115) DEFAULT NULL,
+  `register_school` varchar(110) DEFAULT NULL,
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(140) DEFAULT NULL,
+  `contact` varchar(140) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(140) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-28 23:53:40
